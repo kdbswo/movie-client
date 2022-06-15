1d4b60394b607a9e15131e2be1443df1f3ebd5ff
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-06-16 06:42:07 +0900

    useQuery variables

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,27 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      small_cover_image
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie details</div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    //변수를 필요로하는 쿼리로 변수를 보내는 방법
+    variables: {
+      movieId: id,
+    },
+  });
+  console.log(data, loading);
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return <div>{data.movie.title}</div>;
 }
