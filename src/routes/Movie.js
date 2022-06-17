a406c63e8f64629f351160c10f1e0c305f3bc599
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-06-17 14:08:02 +0900

    local only field

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -9,6 +9,7 @@ const GET_MOVIE = gql`
       title
       medium_cover_image
       rating
+      isLiked @client
     }
   }
 `;
@@ -61,6 +62,7 @@ export default function Movie() {
       <Column>
         <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
         <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <button>{data?.movie?.isLiked ? "Unlike" : "Like"}</button>
       </Column>
       <Image bg={data?.movie?.medium_cover_image} />
     </Container>
